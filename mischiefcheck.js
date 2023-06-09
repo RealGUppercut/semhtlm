8ce5e324cfef35bdddc5fb5ae4ae8d31d49ddec4
JoeVenton <joe.venton@gmail.com>
2023-06-09 15:44:22 +0100

    added sweetalert JS stuff

---
+++ mischiefcheck.js
@@ -0,0 +1,6 @@
+function mischiefcheck() {
+    let ratnumber = prompt("How many rats do you have?",0);
+    if (ratnumber == 1)
+    alert("Lone rats are unhappy rats! Please find your rat a friend ASAP!");
+}
+console.log(mischiefcheck);
