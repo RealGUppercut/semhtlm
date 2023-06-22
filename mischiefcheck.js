b79e2d7909df9de4a5f0d77ad491d4f525edc58b
JoeVenton <joe.venton@gmail.com>
2023-06-22 09:16:34 +0100

    adjusted page

--- mischiefcheck.js
+++ mischiefcheck.js
@@ -1,6 +1,6 @@
 function mischiefcheck() {
     let ratnumber = prompt("How many rats do you have?",0);
     if (ratnumber == 1)
-    alert("Lone rats are unhappy rats! Please find your rat a friend ASAP!");
+     alert("Lone rats are unhappy rats! Please find your rat a frind as soon as possible!");
 }
 console.log(mischiefcheck);
